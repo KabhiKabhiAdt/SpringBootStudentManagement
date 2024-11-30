ad510e743ab023542fbd0fb2699b20f198b861a1
KabhiKabhiAdt <aditis.dis@gmail.com>
2024-11-30 23:13:53 +0530

    Commiting deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+APP_NAME="SpringBootRegistrationLogin"
+JAR_FILE="/home/ubuntu/$APP_NAME.jar"
+APP_DIR="/home/ubuntu/$APP_NAME"
+BACKUP_DIR="/home/ubuntu/backup"
+MYSQL_HOST="localhost"
+MYSQL_USER="newuser"
+MYSQL_PASSWORD="newuser"
+MYSQL_DB="newlogindb"
+
+echo "Stopping existing application..."
+pkill -f $APP_NAME || true
+
+echo "Backing up old JAR..."
+mkdir -p $BACKUP_DIR
+mv $JAR_FILE $BACKUP_DIR/${APP_NAME}_$(date +%F_%T).jar || true
+
+echo "Deploying new JAR..."
+mv /tmp/$APP_NAME.jar $JAR_FILE
+
+echo "Starting application..."
+nohup java -jar $JAR_FILE --spring.datasource.url=jdbc:mysql://$MYSQL_HOST:3306/$MYSQL_DB --spring.datasource.username=$MYSQL_USER --spring.datasource.password=$MYSQL_PASSWORD > /home/ubuntu/$APP_NAME.log 2>&1 &
--- src/main/java/com/aditiSpringProject/SpringBootRegistrationLoginApplication.java
+++ src/main/java/com/aditiSpringProject/SpringBootRegistrationLoginApplication.java
@@ -2,8 +2,10 @@ package com.aditispringproject;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringBootRegistrationLoginApplication {
 
 	public static void main(String[] args) {
